73a5c0b28e7269f9892ec61c4e1048aa8defe03a
IvoryCoding <67837670+IvoryCoding@users.noreply.github.com>
2022-04-27 13:31:18 -0600

    Small edits
    Minor edits

--- linux-server.py
+++ linux-server.py
@@ -4,3 +4,13 @@
 #   Resources:
 
 #!/usr/bin/python3
+
+import json
+import processmodule
+
+def processModule():
+        with open('server-proc.json', 'w', encoding='utf-8') as f:
+                json.dump(processmodule.getProcess(), f, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+        processModule()
