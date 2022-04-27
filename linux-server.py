197fbb568e63e9aae4b9e04dd622ba096e8d8333
IvoryCoding <67837670+IvoryCoding@users.noreply.github.com>
2022-04-27 14:25:37 -0600

    server status
    added a server status module for linux machine. Need to add one for windows and run respective status function

--- linux-server.py
+++ linux-server.py
@@ -1,16 +1,34 @@
 #   Date:        2022/04/27
 #   Author:      Emma Gillespie
 #   Description: A script that gets linux server information and sends it to webserver in the form of json
-#   Resources:
 
 #!/usr/bin/python3
 
+from threading import *
 import json
+
+# Modules to import
 import processmodule
+import statusmodule
 
 def processModule():
+    try:
         with open('server-proc.json', 'w', encoding='utf-8') as f:
-                json.dump(processmodule.getProcess(), f, ensure_ascii=False, indent=4)
+            json.dump(processmodule.getProcess(), f, ensure_ascii=False, indent=4)
+    except:
+        print("Process module failed")
+
+def serverStatusModule():
+    try:
+        with open("server-status.json", "w", encoding="utf-8") as f:
+            json.dump(statusmodule.getStatus(), f, ensure_ascii=False, indent=4)
+    except:
+        print("Status module failed")
 
 if __name__ == "__main__":
-        processModule()
+    # Create threads to run modules
+    pmThread = Thread(target=processModule)
+    pmThread.start()
+
+    ssmThread = Thread(target=serverStatusModule)
+    ssmThread.start()
---
+++ statusmodule.py
@@ -0,0 +1,65 @@
+#   Date:        2022/04/27
+#   Author:      Emma Gillespie
+#   Description: A script that gets status of the server
+
+#!/usr/bin/python3
+
+def getStatus():
+    statusData = {}
+
+    # Computer upload and download speed
+    with open('/proc/net/dev', 'r') as f:
+        for line in f:
+            if 'eth0' in line:
+                download = line.split()[1]
+                upload = line.split()[9]
+
+                download = int(download) / 1024 / 1024
+                upload = int(upload) / 1024 / 1024
+
+                statusData['download'] = download
+                statusData['upload'] = upload
+    
+    # Computer uptime
+    with open('/proc/uptime', 'r') as f:
+        uptime = f.readline().split()[0]
+
+        days = int(uptime) // 86400
+        hours = int(uptime) // 3600 % 24
+        minutes = int(uptime) // 60 % 60
+
+        statusData['uptime'] = days, hours, minutes
+    
+    # Computer memory usage
+    with open('/proc/meminfo', 'r') as f:
+        for line in f:
+            if 'MemTotal' in line:
+                memTotal = line.split()[1]
+            if 'MemFree' in line:
+                memFree = line.split()[1]
+            if 'Buffers' in line:
+                buffers = line.split()[1]
+            if 'Cached' in line:
+                cached = line.split()[1]
+
+        memTotal = int(memTotal) / 1024
+        memFree = int(memFree) / 1024
+        buffers = int(buffers) / 1024
+        cached = int(cached) / 1024
+
+        statusData['memTotal'] = memTotal
+        statusData['memFree'] = memFree
+        statusData['buffers'] = buffers
+        statusData['cached'] = cached
+    
+    # Computer CPU usage
+    with open('/proc/stat', 'r') as f:
+        for line in f:
+            if 'cpu' in line:
+                cpu = line.split()[1]
+
+        cpu = int(cpu) / 100
+
+        statusData['cpu'] = cpu
+
+    return statusData
